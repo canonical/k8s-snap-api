87e4cbb0912698cb54976b1cd1b3da28c7160cc4
Benjamin Schimke <benjamin.schimke@canonical.com>
2024-09-19 14:24:25 +0200

    add request message

--- api/v1/rpc_certificate_expiry.go
+++ api/v1/rpc_certificate_expiry.go
@@ -3,6 +3,10 @@ package apiv1
 // ClusterAPICertificatesExpiryRPC is the path for the ClusterAPICertificatesExpiry RPC.
 const ClusterAPICertificatesExpiryRPC = "x/capi/certificates-expiry"
 
+// CertificatesExpiryRequest is the request message for the CertificatesExpiry RPC.
+type CertificatesExpiryRequest struct{}
+
+// CertificatesExpiryResponse is the response message for the CertificatesExpiry RPC.
 type CertificatesExpiryResponse struct {
 	// ExpiryDate is the expiry date of the certificates on the node.
 	ExpiryDate string `json:"expiry-date"`
