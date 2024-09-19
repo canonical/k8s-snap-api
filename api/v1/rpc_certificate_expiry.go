006facf788a709eb2ccbb1b4ee7d9e2d81e53b72
Benjamin Schimke <benjamin.schimke@canonical.com>
2024-09-19 15:41:36 +0200

    Update api/v1/rpc_certificate_expiry.go
    Co-authored-by: Mateo Florido <32885896+mateoflorido@users.noreply.github.com>

--- api/v1/rpc_certificate_expiry.go
+++ api/v1/rpc_certificate_expiry.go
@@ -8,6 +8,6 @@ type CertificatesExpiryRequest struct{}
 
 // CertificatesExpiryResponse is the response message for the CertificatesExpiry RPC.
 type CertificatesExpiryResponse struct {
-	// ExpiryDate is the expiry date of the certificates on the node.
+	// ExpiryDate is the expiry date of the certificates on the node in RFC3339 format.
 	ExpiryDate string `json:"expiry-date"`
 }
