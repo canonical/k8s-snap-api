c0288171357789297722ad9e9adbabf7b1e5d3ce
Benjamin Schimke <benjamin.schimke@canonical.com>
2024-09-19 11:18:36 +0200

    Add skip services stop annotation (#8)
    * add skip services stop annotation
    * add annotations to worker response

--- api/v1/const.go
+++ api/v1/const.go
@@ -8,4 +8,9 @@ const (
 	// This is useful, if an external controller (e.g. CAPI) is responsible for the Kubernetes node life cycle.
 	// By default, the Kubernetes node is removed by k8sd if a node is removed from the cluster.
 	AnnotationSkipCleanupKubernetesNodeOnRemove = "k8sd/v1alpha/lifecycle/skip-cleanup-kubernetes-node-on-remove"
+
+	// AnnotationSkipStopServicesOnRemove if set, the k8s services will not be stopped on the leaving node when removing the node.
+	// This is useful, if an external controller (e.g. CAPI) is responsible for the Kubernetes node life cycle.
+	// By default, all services are stopped on leaving nodes.
+	AnnotationSkipStopServicesOnRemove = "k8sd/v1alpha/lifecycle/skip-stop-services-on-remove"
 )
