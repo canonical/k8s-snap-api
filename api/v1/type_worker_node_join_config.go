1d0eecc148419361605269c5c12714d646b82e0b
Lucian Petrut <petrutlucian94@gmail.com>
2024-10-04 13:01:08 +0300

    Add field docstrings, to be used by doc automation (#11)
    * Add field docstrings, to be used by doc automation
    
    The bootstrap config documentation can easily get out of sync, for
    which reason we intend to automatically generate the documentation
    based on structure docstrings.
    
    To do so, we're copying over the field descriptions from the
    following document:
    
    * https://github.com/canonical/k8s-snap/blob/ee017318d958b4c7ed1ab78d3cf4d8b7e94559c5/docs/src/snap/reference/bootstrap-config-reference.md
    
    A separate PR adds the necessary automation. Check
    docs/src/_parts/bootstrap_config.md for a sample of the generated
    Markdown document.
    
    * https://github.com/canonical/k8s-snap/pull/711
    
    Note that in order to be picked up, the inline comments must be
    placed immediately before the corresponding structure field.
    
    Certain comments may only be intended for developers, just add a
    new line to avoid including these comments in the generated
    documentation:
    
    ```
        type SomeStruct struct {
            // skipped comment
    
            SomeField int `json:"some-field"`
    
            // multiline comment
            //
            // of a given field
            OtherField string `json:"other-field"`
        }
    ```
    
    * Document cluster join structures
    
    These structures are mostly subsets of the bootstrap config structure,
    so we can just reuse the same docstrings.

--- api/v1/type_worker_node_join_config.go
+++ api/v1/type_worker_node_join_config.go
@@ -3,20 +3,49 @@ package apiv1
 import "github.com/canonical/k8s-snap-api/internal/util"
 
 type WorkerJoinConfig struct {
+	// The certificate to be used for the kubelet.
+	// If omitted defaults to an auto generated certificate.
 	KubeletCert         *string `json:"kubelet-crt,omitempty" yaml:"kubelet-crt,omitempty"`
+	// The key to be used for the kubelet.
+	// If omitted defaults to an auto generated key.
 	KubeletKey          *string `json:"kubelet-key,omitempty" yaml:"kubelet-key,omitempty"`
+	// The client certificate to be used for the kubelet.
+	// If omitted defaults to an auto generated certificate.
 	KubeletClientCert   *string `json:"kubelet-client-crt,omitempty" yaml:"kubelet-client-crt,omitempty"`
+	// The client key to be used for the kubelet.
+	// If omitted defaults to an auto generated key.
 	KubeletClientKey    *string `json:"kubelet-client-key,omitempty" yaml:"kubelet-client-key,omitempty"`
+	// The client certificate to be used for the kube-proxy.
+	// If omitted defaults to an auto generated certificate.
 	KubeProxyClientCert *string `json:"kube-proxy-client-crt,omitempty" yaml:"kube-proxy-client-crt,omitempty"`
+	// The client key to be used for the kube-proxy.
+	// If omitted defaults to an auto generated key.
 	KubeProxyClientKey  *string `json:"kube-proxy-client-key,omitempty" yaml:"kube-proxy-client-key,omitempty"`
 
-	// ExtraNodeConfigFiles will be written to /var/snap/k8s/common/args/conf.d
+	// Additional files that are uploaded `/var/snap/k8s/common/args/conf.d/<filename>`
+	// to a node on bootstrap. These files can then be referenced by Kubernetes
+	// service arguments.
+	//
+	// The format is `map[<filename>]<filecontent>`.
 	ExtraNodeConfigFiles map[string]string `json:"extra-node-config-files,omitempty" yaml:"extra-node-config-files,omitempty"`
 
 	// Extra args to add to individual services (set any arg to null to delete)
+
+	// Additional arguments that are passed to the `kube-proxy` only for that specific node.
+	// A parameter that is explicitly set to `null` is deleted.
+	// The format is `map[<--flag-name>]<value>`.
 	ExtraNodeKubeProxyArgs         map[string]*string `json:"extra-node-kube-proxy-args,omitempty" yaml:"extra-node-kube-proxy-args,omitempty"`
+	// Additional arguments that are passed to the `kubelet` only for that specific node.
+	// A parameter that is explicitly set to `null` is deleted.
+	// The format is `map[<--flag-name>]<value>`.
 	ExtraNodeKubeletArgs           map[string]*string `json:"extra-node-kubelet-args,omitempty" yaml:"extra-node-kubelet-args,omitempty"`
+	// Additional arguments that are passed to `containerd` only for that specific node.
+	// A parameter that is explicitly set to `null` is deleted.
+	// The format is `map[<--flag-name>]<value>`.
 	ExtraNodeContainerdArgs        map[string]*string `json:"extra-node-containerd-args,omitempty" yaml:"extra-node-containerd-args,omitempty"`
+	// Additional arguments that are passed to `k8s-api-server-proxy` only for that specific node.
+	// A parameter that is explicitly set to `null` is deleted.
+	// The format is `map[<--flag-name>]<value>`.
 	ExtraNodeK8sAPIServerProxyArgs map[string]*string `json:"extra-node-k8s-apiserver-proxy-args,omitempty" yaml:"extra-node-k8s-apiserver-proxy-args,omitempty"`
 
 	// Extra configuration for the containerd config.toml
