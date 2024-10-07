b4f2967a80a89613864cf51252081bbb409e921e
Mateo Florido <mateo.florido@canonical.com>
2024-10-06 22:38:01 -0500

    Add CAPI approve endpoints

---
+++ api/v1/rpc_clusterapi_certificates_approve.go
@@ -0,0 +1,11 @@
+package apiv1
+
+const ClusterAPIApproveWorkerCSRRPC = "x/capi/refresh-certs/approve"
+
+// ClusterAPIApproveWorkerCSRRequest is the request message for the ClusterAPIApproveWorkerCSR RPC.
+type ClusterAPIApproveWorkerCSRRequest struct {
+	Seed int `json:"seed"`
+}
+
+// ClusterAPIApproveWorkerCSRResponse is the response message for the ClusterAPIApproveWorkerCSR RPC.
+type ClusrterAPIApproveWorkerCSRResponse struct{}
