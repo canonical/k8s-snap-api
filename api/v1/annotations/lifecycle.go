139b1877af09e4a226110a72542e168f94d9b8fd
Berkay Tekin Öz <berkay.tekinoz@canonical.com>
2024-10-16 14:23:09 +0300

    Move feature annotations under the api repository (#13)

--- api/v1/annotations/lifecycle.go
+++ api/v1/annotations/lifecycle.go
@@ -1,20 +1,13 @@
-package apiv1
+package annotations
 
 const (
-	// K8sdAPIVersion is the path prefix that will be used for the k8sd endpoints for this api version.
-	K8sdAPIVersion = "1.0"
-
 	// AnnotationSkipCleanupKubernetesNodeOnRemove if set, only the microcluster & file cleanup is done.
 	// This is useful, if an external controller (e.g. CAPI) is responsible for the Kubernetes node life cycle.
 	// By default, the Kubernetes node is removed by k8sd if a node is removed from the cluster.
-	//
-	// Deprecated: Use the respective annotation from annotations package instead.
 	AnnotationSkipCleanupKubernetesNodeOnRemove = "k8sd/v1alpha/lifecycle/skip-cleanup-kubernetes-node-on-remove"
 
 	// AnnotationSkipStopServicesOnRemove if set, the k8s services will not be stopped on the leaving node when removing the node.
 	// This is useful, if an external controller (e.g. CAPI) is responsible for the Kubernetes node life cycle.
 	// By default, all services are stopped on leaving nodes.
-	//
-	// Deprecated: Use the respective annotation from annotations package instead.
 	AnnotationSkipStopServicesOnRemove = "k8sd/v1alpha/lifecycle/skip-stop-services-on-remove"
 )
