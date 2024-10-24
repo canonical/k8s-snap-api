e43093fa2fc23a7fa3d6e51b11820cb8a60cbc2a
Berkay Tekin Öz <berkay.tekinoz@canonical.com>
2024-10-24 10:37:10 +0300

    Add annotation docstrings for feature annotations (#15)

--- api/v1/annotations/metrics-server/annotations.go
+++ api/v1/annotations/metrics-server/annotations.go
@@ -1,6 +1,8 @@
 package metrics_server
 
 const (
+	// Override the default image repository for the metrics-server.
 	AnnotationImageRepo = "k8sd/v1alpha1/metrics-server/image-repo"
-	AnnotationImageTag  = "k8sd/v1alpha1/metrics-server/image-tag"
+	// Override the default image tag for the metrics-server.
+	AnnotationImageTag = "k8sd/v1alpha1/metrics-server/image-tag"
 )
--- api/v1/type_cluster_config.go
+++ api/v1/type_cluster_config.go
@@ -9,17 +9,17 @@ import (
 
 type UserFacingClusterConfig struct {
 	// Configuration options for the network feature.
-	Network       NetworkConfig       `json:"network,omitempty" yaml:"network,omitempty"`
+	Network NetworkConfig `json:"network,omitempty" yaml:"network,omitempty"`
 	// Configuration options for the dns feature.
-	DNS           DNSConfig           `json:"dns,omitempty" yaml:"dns,omitempty"`
+	DNS DNSConfig `json:"dns,omitempty" yaml:"dns,omitempty"`
 	// Configuration options for the ingress feature.
-	Ingress       IngressConfig       `json:"ingress,omitempty" yaml:"ingress,omitempty"`
+	Ingress IngressConfig `json:"ingress,omitempty" yaml:"ingress,omitempty"`
 	// Configuration options for the load-balancer feature.
-	LoadBalancer  LoadBalancerConfig  `json:"load-balancer,omitempty" yaml:"load-balancer,omitempty"`
+	LoadBalancer LoadBalancerConfig `json:"load-balancer,omitempty" yaml:"load-balancer,omitempty"`
 	// Configuration options for the local-storage feature.
-	LocalStorage  LocalStorageConfig  `json:"local-storage,omitempty" yaml:"local-storage,omitempty"`
+	LocalStorage LocalStorageConfig `json:"local-storage,omitempty" yaml:"local-storage,omitempty"`
 	// Configuration options for the gateway feature.
-	Gateway       GatewayConfig       `json:"gateway,omitempty" yaml:"gateway,omitempty"`
+	Gateway GatewayConfig `json:"gateway,omitempty" yaml:"gateway,omitempty"`
 	// Configuration options for the metric server feature.
 	MetricsServer MetricsServerConfig `json:"metrics-server,omitempty" yaml:"metrics-server,omitempty"`
 	// Sets the cloud provider to be used by the cluster.
@@ -28,24 +28,24 @@ type UserFacingClusterConfig struct {
 	// do cloud specific setup and finish node initialization.
 	//
 	// Possible values: `external`.
-	CloudProvider *string             `json:"cloud-provider,omitempty" yaml:"cloud-provider,omitempty"`
+	CloudProvider *string `json:"cloud-provider,omitempty" yaml:"cloud-provider,omitempty"`
 	// Annotations is a map of strings that can be used to store arbitrary metadata configuration.
-	// Please refer to the ClusterAPI annotations reference for further details on these options.
-	Annotations   map[string]string   `json:"annotations,omitempty" yaml:"annotations,omitempty"`
+	// Please refer to the annotations reference for further details on these options.
+	Annotations map[string]string `json:"annotations,omitempty" yaml:"annotations,omitempty"`
 }
 
 type DNSConfig struct {
 	// Determines if the feature should be enabled.
 	// If omitted defaults to `true`
-	Enabled             *bool     `json:"enabled,omitempty" yaml:"enabled,omitempty"`
+	Enabled *bool `json:"enabled,omitempty" yaml:"enabled,omitempty"`
 	// Sets the local domain of the cluster.
 	// If omitted defaults to `cluster.local`.
-	ClusterDomain       *string   `json:"cluster-domain,omitempty" yaml:"cluster-domain,omitempty"`
+	ClusterDomain *string `json:"cluster-domain,omitempty" yaml:"cluster-domain,omitempty"`
 	// Sets the IP address of the dns service. If omitted defaults to the IP address
 	// of the Kubernetes service created by the feature.
 	//
 	// Can be used to point to an external dns server when feature is disabled.
-	ServiceIP           *string   `json:"service-ip,omitempty" yaml:"service-ip,omitempty"`
+	ServiceIP *string `json:"service-ip,omitempty" yaml:"service-ip,omitempty"`
 	// Sets the upstream nameservers used to forward queries for out-of-cluster
 	// endpoints.
 	//
@@ -61,16 +61,16 @@ func (c DNSConfig) GetUpstreamNameservers() []string { return util.Deref(c.Upstr
 type IngressConfig struct {
 	// Determines if the feature should be enabled.
 	// If omitted defaults to `false`
-	Enabled             *bool   `json:"enabled,omitempty" yaml:"enabled,omitempty"`
+	Enabled *bool `json:"enabled,omitempty" yaml:"enabled,omitempty"`
 	// Sets the name of the secret to be used for providing default encryption to
 	// ingresses.
 	//
 	// Ingresses can specify another TLS secret in their resource definitions,
 	// in which case the default secret won't be used.
-	DefaultTLSSecret    *string `json:"default-tls-secret,omitempty" yaml:"default-tls-secret,omitempty"`
+	DefaultTLSSecret *string `json:"default-tls-secret,omitempty" yaml:"default-tls-secret,omitempty"`
 	// Determines if the proxy protocol should be enabled for ingresses.
 	// If omitted defaults to `false`.
-	EnableProxyProtocol *bool   `json:"enable-proxy-protocol,omitempty" yaml:"enable-proxy-protocol,omitempty"`
+	EnableProxyProtocol *bool `json:"enable-proxy-protocol,omitempty" yaml:"enable-proxy-protocol,omitempty"`
 }
 
 func (c IngressConfig) GetEnabled() bool             { return util.Deref(c.Enabled) }
@@ -80,31 +80,31 @@ func (c IngressConfig) GetEnableProxyProtocol() bool { return util.Deref(c.Enabl
 type LoadBalancerConfig struct {
 	// Determines if the feature should be enabled.
 	// If omitted defaults to `false`.
-	Enabled        *bool     `json:"enabled,omitempty" yaml:"enabled,omitempty"`
+	Enabled *bool `json:"enabled,omitempty" yaml:"enabled,omitempty"`
 	// Sets the CIDRs used for assigning IP addresses to Kubernetes services with type
 	// `LoadBalancer`.
-	CIDRs          *[]string `json:"cidrs,omitempty" yaml:"cidrs,omitempty"`
+	CIDRs *[]string `json:"cidrs,omitempty" yaml:"cidrs,omitempty"`
 	// Determines if L2 mode should be enabled.
 	// If omitted defaults to `false`.
-	L2Mode         *bool     `json:"l2-mode,omitempty" yaml:"l2-mode,omitempty"`
+	L2Mode *bool `json:"l2-mode,omitempty" yaml:"l2-mode,omitempty"`
 	// Sets the interfaces to be used for announcing IP addresses through ARP.
 	// If omitted all interfaces will be used.
-	L2Interfaces   *[]string `json:"l2-interfaces,omitempty" yaml:"l2-interfaces,omitempty"`
+	L2Interfaces *[]string `json:"l2-interfaces,omitempty" yaml:"l2-interfaces,omitempty"`
 	// Determines if BGP mode should be enabled.
 	// If omitted defaults to `false`.
-	BGPMode        *bool     `json:"bgp-mode,omitempty" yaml:"bgp-mode,omitempty"`
+	BGPMode *bool `json:"bgp-mode,omitempty" yaml:"bgp-mode,omitempty"`
 	// Sets the ASN to be used for the local virtual BGP router.
 	// Required if bgp-mode is true.
-	BGPLocalASN    *int      `json:"bgp-local-asn,omitempty" yaml:"bgp-local-asn,omitempty"`
+	BGPLocalASN *int `json:"bgp-local-asn,omitempty" yaml:"bgp-local-asn,omitempty"`
 	// Sets the IP address of the BGP peer.
 	// Required if bgp-mode is true.
-	BGPPeerAddress *string   `json:"bgp-peer-address,omitempty" yaml:"bgp-peer-address,omitempty"`
+	BGPPeerAddress *string `json:"bgp-peer-address,omitempty" yaml:"bgp-peer-address,omitempty"`
 	// Sets the ASN of the BGP peer.
 	// Required if bgp-mode is true.
-	BGPPeerASN     *int      `json:"bgp-peer-asn,omitempty" yaml:"bgp-peer-asn,omitempty"`
+	BGPPeerASN *int `json:"bgp-peer-asn,omitempty" yaml:"bgp-peer-asn,omitempty"`
 	// Sets the port of the BGP peer.
 	// Required if bgp-mode is true.
-	BGPPeerPort    *int      `json:"bgp-peer-port,omitempty" yaml:"bgp-peer-port,omitempty"`
+	BGPPeerPort *int `json:"bgp-peer-port,omitempty" yaml:"bgp-peer-port,omitempty"`
 }
 
 func (c LoadBalancerConfig) GetEnabled() bool          { return util.Deref(c.Enabled) }
@@ -120,17 +120,17 @@ func (c LoadBalancerConfig) GetBGPPeerPort() int       { return util.Deref(c.BGP
 type LocalStorageConfig struct {
 	// Determines if the feature should be enabled.
 	// If omitted defaults to `false`.
-	Enabled       *bool   `json:"enabled,omitempty" yaml:"enabled,omitempty"`
+	Enabled *bool `json:"enabled,omitempty" yaml:"enabled,omitempty"`
 	// Sets the path to be used for storing volume data.
 	// If omitted defaults to `/var/snap/k8s/common/rawfile-storage`
-	LocalPath     *string `json:"local-path,omitempty" yaml:"local-path,omitempty"`
+	LocalPath *string `json:"local-path,omitempty" yaml:"local-path,omitempty"`
 	// Sets the reclaim policy of the storage class.
 	// If omitted defaults to `Delete`.
 	// Possible values: `Retain | Recycle | Delete`
 	ReclaimPolicy *string `json:"reclaim-policy,omitempty" yaml:"reclaim-policy,omitempty"`
 	// Determines if the storage class should be set as default.
 	// If omitted defaults to `true`
-	Default       *bool   `json:"default,omitempty" yaml:"default,omitempty"`
+	Default *bool `json:"default,omitempty" yaml:"default,omitempty"`
 }
 
 func (c LocalStorageConfig) GetEnabled() bool         { return util.Deref(c.Enabled) }
@@ -164,15 +164,15 @@ func (c MetricsServerConfig) GetEnabled() bool { return util.Deref(c.Enabled) }
 
 type UserFacingDatastoreConfig struct {
 	// Type of the datastore. Needs to be "external".
-	Type       *string   `json:"type,omitempty" yaml:"type,omitempty"`
+	Type *string `json:"type,omitempty" yaml:"type,omitempty"`
 	// Datastore server addresses.
-	Servers    *[]string `json:"servers,omitempty" yaml:"servers,omitempty"`
+	Servers *[]string `json:"servers,omitempty" yaml:"servers,omitempty"`
 	// Datastore CA certificate.
-	CACert     *string   `json:"ca-crt,omitempty" yaml:"ca-crt,omitempty"`
+	CACert *string `json:"ca-crt,omitempty" yaml:"ca-crt,omitempty"`
 	// Datastore client certificate.
-	ClientCert *string   `json:"client-crt,omitempty" yaml:"client-crt,omitempty"`
+	ClientCert *string `json:"client-crt,omitempty" yaml:"client-crt,omitempty"`
 	// Datastore client key.
-	ClientKey  *string   `json:"client-key,omitempty" yaml:"client-key,omitempty"`
+	ClientKey *string `json:"client-key,omitempty" yaml:"client-key,omitempty"`
 }
 
 func (c UserFacingDatastoreConfig) GetType() string       { return util.Deref(c.Type) }
