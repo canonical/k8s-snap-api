d96d2ca9e11e573d867d6382ad2c58201a26d90a
Claudiu Belu <claudiu.belu@canonical.com>
2025-02-18 10:47:34 +0200

    Fix bootstrap config field (#26)
    Currently, the BootstrapConfig.KubeControllerManagerClientKey can be
    unmarshaled from the kube-controller-manager-client-key json field and
    kube-ControllerManager-client-key yaml field, which is inconsistent.
    
    Since this is meant to be an API, old value has to be supported for a
    while as well. There is no direct support for handling 2 keys for the
    same field, so this employs a workaround for it.

--- api/v1/type_worker_node_join_config.go
+++ api/v1/type_worker_node_join_config.go
@@ -5,22 +5,22 @@ import "github.com/canonical/k8s-snap-api/internal/util"
 type WorkerJoinConfig struct {
 	// The certificate to be used for the kubelet.
 	// If omitted defaults to an auto generated certificate.
-	KubeletCert         *string `json:"kubelet-crt,omitempty" yaml:"kubelet-crt,omitempty"`
+	KubeletCert *string `json:"kubelet-crt,omitempty" yaml:"kubelet-crt,omitempty"`
 	// The key to be used for the kubelet.
 	// If omitted defaults to an auto generated key.
-	KubeletKey          *string `json:"kubelet-key,omitempty" yaml:"kubelet-key,omitempty"`
+	KubeletKey *string `json:"kubelet-key,omitempty" yaml:"kubelet-key,omitempty"`
 	// The client certificate to be used for the kubelet.
 	// If omitted defaults to an auto generated certificate.
-	KubeletClientCert   *string `json:"kubelet-client-crt,omitempty" yaml:"kubelet-client-crt,omitempty"`
+	KubeletClientCert *string `json:"kubelet-client-crt,omitempty" yaml:"kubelet-client-crt,omitempty"`
 	// The client key to be used for the kubelet.
 	// If omitted defaults to an auto generated key.
-	KubeletClientKey    *string `json:"kubelet-client-key,omitempty" yaml:"kubelet-client-key,omitempty"`
+	KubeletClientKey *string `json:"kubelet-client-key,omitempty" yaml:"kubelet-client-key,omitempty"`
 	// The client certificate to be used for the kube-proxy.
 	// If omitted defaults to an auto generated certificate.
 	KubeProxyClientCert *string `json:"kube-proxy-client-crt,omitempty" yaml:"kube-proxy-client-crt,omitempty"`
 	// The client key to be used for the kube-proxy.
 	// If omitted defaults to an auto generated key.
-	KubeProxyClientKey  *string `json:"kube-proxy-client-key,omitempty" yaml:"kube-proxy-client-key,omitempty"`
+	KubeProxyClientKey *string `json:"kube-proxy-client-key,omitempty" yaml:"kube-proxy-client-key,omitempty"`
 
 	// Additional files that are uploaded `/var/snap/k8s/common/args/conf.d/<filename>`
 	// to a node on bootstrap. These files can then be referenced by Kubernetes
@@ -34,15 +34,15 @@ type WorkerJoinConfig struct {
 	// Additional arguments that are passed to the `kube-proxy` only for that specific node.
 	// A parameter that is explicitly set to `null` is deleted.
 	// The format is `map[<--flag-name>]<value>`.
-	ExtraNodeKubeProxyArgs         map[string]*string `json:"extra-node-kube-proxy-args,omitempty" yaml:"extra-node-kube-proxy-args,omitempty"`
+	ExtraNodeKubeProxyArgs map[string]*string `json:"extra-node-kube-proxy-args,omitempty" yaml:"extra-node-kube-proxy-args,omitempty"`
 	// Additional arguments that are passed to the `kubelet` only for that specific node.
 	// A parameter that is explicitly set to `null` is deleted.
 	// The format is `map[<--flag-name>]<value>`.
-	ExtraNodeKubeletArgs           map[string]*string `json:"extra-node-kubelet-args,omitempty" yaml:"extra-node-kubelet-args,omitempty"`
+	ExtraNodeKubeletArgs map[string]*string `json:"extra-node-kubelet-args,omitempty" yaml:"extra-node-kubelet-args,omitempty"`
 	// Additional arguments that are passed to `containerd` only for that specific node.
 	// A parameter that is explicitly set to `null` is deleted.
 	// The format is `map[<--flag-name>]<value>`.
-	ExtraNodeContainerdArgs        map[string]*string `json:"extra-node-containerd-args,omitempty" yaml:"extra-node-containerd-args,omitempty"`
+	ExtraNodeContainerdArgs map[string]*string `json:"extra-node-containerd-args,omitempty" yaml:"extra-node-containerd-args,omitempty"`
 	// Additional arguments that are passed to `k8s-api-server-proxy` only for that specific node.
 	// A parameter that is explicitly set to `null` is deleted.
 	// The format is `map[<--flag-name>]<value>`.
--- go.mod
+++ go.mod
@@ -2,4 +2,14 @@ module github.com/canonical/k8s-snap-api
 
 go 1.22.6
 
-require gopkg.in/yaml.v2 v2.4.0
+require (
+	github.com/onsi/gomega v1.36.2
+	gopkg.in/yaml.v2 v2.4.0
+)
+
+require (
+	github.com/google/go-cmp v0.6.0 // indirect
+	golang.org/x/net v0.33.0 // indirect
+	golang.org/x/text v0.21.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
