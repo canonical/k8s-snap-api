79fbe747a63dff1b49a9aecf9dbf739fb47dc4b2
Homayoon (Hue) Alimohammadi <homayoon.alimohammadi@canonical.com>
2025-02-28 18:29:15 +0400

    feat: Update NodeStatus and ClusterConfig response types

--- api/v1/rpc_get_cluster_config.go
+++ api/v1/rpc_get_cluster_config.go
@@ -8,5 +8,8 @@ type GetClusterConfigRequest struct{}
 
 // GetClusterConfigResponse is the response message for the GetClusterConfig RPC.
 type GetClusterConfigResponse struct {
+	// Config is the cluster configuration.
 	Config UserFacingClusterConfig `json:"status"`
+	// Datastore is the datastore configuration.
+	Datastore UserFacingDatastoreConfig `json:"datastore,omitempty" yaml:"datastore,omitempty"`
 }
