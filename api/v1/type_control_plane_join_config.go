37b6939d81341f2f95708fc13d07530b14faa27e
Benjamin Schimke <benjamin.schimke@canonical.com>
2026-01-26 12:41:34 +0100

    docs: ControlPlaneJoinConfig k8s-dqlite deprecation (#48)
    (cherry picked from commit 81468edc84241f931e99af3e641ea983e22f1278)

--- api/v1/type_control_plane_join_config.go
+++ api/v1/type_control_plane_join_config.go
@@ -106,6 +106,9 @@ type ControlPlaneJoinConfig struct {
 	// A parameter that is explicitly set to `null` is deleted.
 	// The format is `map[<--flag-name>]<value>`.
 	ExtraNodeContainerdArgs map[string]*string `json:"extra-node-containerd-args,omitempty" yaml:"extra-node-containerd-args,omitempty"`
+	// Deprecated: k8s-dqlite is being deprecated and will be removed in Canonical Kubernetes 1.36 without an upgrade path.
+	// We recommend against bootstrapping new clusters with k8s-dqlite.
+	//
 	// Additional arguments that are passed to `k8s-dqlite` only for that specific node.
 	// A parameter that is explicitly set to `null` is deleted.
 	// The format is `map[<--flag-name>]<value>`.
